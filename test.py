071ec517130638276982b5ef162db8ae0449f8a6
pzo <pierrezero@hotmail.com>
2013-03-08 14:26:13 +0100

    test csv

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 import csv
+from pyparsing import Literal, CaselessLiteral, Word, Upcase, delimitedList, Optional, \
+    Combine, Group, alphas, nums, alphanums, ParseException, Forward, oneOf, quotedString, \
+    ZeroOrMore, restOfLine, Keyword
+
+def test( str ):
+    print(str,"->")
+    try:
+        tokens = simpleSQL.parseString(str)
+        print("tokens = ",        tokens)
+        print("tokens.columns =", tokens.columns)
+        print("tokens.tables =",  tokens.tables)
+        print("tokens.where =", tokens.where)
+    except (ParseException, err):
+        print(" "*err.loc + "^\n" + err.msg)
+        print(err)
+    print
 
 a = """"F1", "F2", "F3", "F4", "F5"
 "A",2,3,4,5
@@ -11,3 +27,23 @@ for row in r:
 	print(row)
 
 print("Hello, World!")
+
+# define SQL tokens
+selectStmt = Forward()
+selectToken = Keyword("select", caseless=True)
+fromToken   = Keyword("from", caseless=True)
+
+ident          = Word( alphas, alphanums + "_$" ).setName("identifier")
+columnName     = Upcase( delimitedList( ident, ".", combine=True ) )
+columnNameList = Group( delimitedList( columnName ) )
+tableName      = Upcase( delimitedList( ident, ".", combine=True ) )
+tableNameList  = Group( delimitedList( tableName ) )
+
+selectStmt      << ( selectToken +
+                   ( '*' | columnNameList ).setResultsName( "columns" ) +
+                   fromToken +
+                   tableNameList.setResultsName( "tables" ) )
+
+simpleSQL = selectStmt
+test("select f1,f5 from csv")
+
