1b549b4261b4b7cd90d3ff9cae6697f6f4072737
pzo <pierrezero@hotmail.com>
2013-03-08 14:11:02 +0100

    test csv

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+import csv
+
+a = """"F1", "F2", "F3", "F4", "F5"
+"A",2,3,4,5
+"B",6,7,8,9
+"""
+
+r = csv.DictReader(a.split("\n"), dialect = csv.excel)
+for row in r:
+	print(row)
+
 print("Hello, World!")
