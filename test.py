8b233b80d37fe172225fa4768d87ca4d65aba5db
pzo <pierrezero@hotmail.com>
2013-03-14 15:14:55 +0100

    pep8

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
-
 import csv
 import sqlparser
 
-def test( str ):
-	print("\n", str,"->")
-	tokens = sqlparser.parse(str)
-	if tokens:
-		print("tokens = ",        tokens)
-		print("tokens.columns =", tokens.columns)
-		print("tokens.tables =",  tokens.tables)
-		print("tokens.where =", tokens.where)
 
-		csvstr = """F1,F2,F3,F4,F5
+def test(str):
+    print("\n", str, "->")
+    tokens = sqlparser.parse(str)
+    if tokens:
+        print("tokens = ",        tokens)
+        print("tokens.columns =", tokens.columns)
+        print("tokens.tables =",  tokens.tables)
+        print("tokens.where =",   tokens.where)
+
+        csvstr = """F1,F2,F3,F4,F5
 "A",2,3,4,5
 "B",6,7,8,9
 """
-		r = csv.DictReader(csvstr.split("\n"))
-		for row in r:
-			for col in tokens.columns:
-				print(row[col], end="")
-			print()
+        r = csv.DictReader(csvstr.split("\n"))
+        for row in r:
+            for col in tokens.columns:
+                print(row[col], end="")
+            print()
 
-		print
+        print
 
 print("Hello, World!")
 
